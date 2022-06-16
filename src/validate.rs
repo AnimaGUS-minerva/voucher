a86d74bd0c6b073a5183f6eac83b1afc03962305
j-devel <38098238+j-devel@users.noreply.github.com>
2022-06-16 10:16:30 +0700

    Fix `validate_with_mbedtls()` potential crash on calling `ecp_point::read_binary()`

--- src/validate.rs
+++ src/validate.rs
@@ -61,11 +61,11 @@ pub fn validate_with_mbedtls(
     } else if let Some(cert) = signer_cert {
         let grp = ecp_group::from_id(ECP_DP_SECP256R1)?;
         let mut pt = ecp_point::new();
-        pt.read_binary(&grp, cert)?;
+        pt.read_binary(grp, cert)?;
 
         pk_context::new()
             .setup(PK_ECKEY)?
-            .set_grp(grp)
+            .set_grp(ecp_group::from_id(ECP_DP_SECP256R1)?)
             .set_q(pt)
             .verify(md_ty, hash, signature)
     } else {
